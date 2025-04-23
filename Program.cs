4f4d5cc02a6e8336bb9c6c8a365f4d34cb91b20d
MSBabaei1384 <saleh.byb@gmail.com>
2025-04-23 21:58:10 +0330

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,493 +1,410 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
 
 //1
 
-//int Pairsum =0;
-//int Oddsum=0;
+//int number = 0;
+//int check = 0;
+//int[]list= {10,11,89,34,5,67,12,55,32,90};
 
-//for (int i = 10; i < 100; i++)
+//Console.Write("Enter your number:");
+//number=int.Parse(Console.ReadLine());
+
+//for(int i = 0; i <list.Length; i++)
 //{
-//    if (i % 2 == 0)
+//    if(number == list[i])
 //    {
-//        Pairsum = Pairsum + i;
-//    }
-//    else
-//    {
-//        Oddsum=Oddsum + i;
+//        Console.WriteLine("Number index:"+i);
+//        check = 1; 
+//        break;
 //    }
 //}
-//Console.WriteLine("Sum of pair numbers:"+Pairsum);
-//Console.WriteLine("Sum of individual numbers:"+Oddsum);
+
+//if(check != 1)
+//{
+//    Console.WriteLine("ERROR");
+//}
+
+
 
 //2
 
 //int number = 0;
-
-//Console.Write("Enter your number:");
-//number=int.Parse(Console.ReadLine());
-//for (int i = 1; i <= number; i++)
-//{
-//    if (i % 2 == 0) 
-//    {
-//        Console.WriteLine(Math.Pow(i,2));
-//    }
-//}
-
-//3
-
-//int number =0;
-//int Hundredsofdigits =0;
-//int Tenthdigit = 0;
-//int Onedigit = 0;
-//double Hundredsofdigits3 = 0;
-//double Tenthdigit3 = 0;
-//double Onedigit3 = 0;
-//double sum = 0;
+//int check = 0;
+//int[] list = { 10, 11, 89, 34, 5, 67, 12, 55, 32, 90 };
+//int[] newlist =new int[10];
 
 //Console.Write("Enter your number:");
 //number = int.Parse(Console.ReadLine());
 
-//Hundredsofdigits = (number % 1000) / 100;
-//Tenthdigit = (number % 100) / 10;
-//Onedigit = number % 10;
-
-//Hundredsofdigits3 = Math.Pow(Hundredsofdigits, 3);
-//Tenthdigit3=Math.Pow(Tenthdigit, 3);
-//Onedigit3 = Math.Pow(Onedigit,3);
-
-//sum = Hundredsofdigits3 + Tenthdigit3 + Onedigit3;
-
-//if (sum == number)
+//for (int i = 0; i < list.Length; i++)
 //{
-//    Console.WriteLine("It is Armstrong number");
+//    if (list[i] != number)
+//    {
+//        newlist[i] = list[i];
+//    }
 //}
-//else
+
+//for (int j = 0;j < list.Length; j++)
 //{
-//    Console.WriteLine("It is Not Armstrong number");
+//    Console.WriteLine(newlist[j]);
 //}
+
+
+
+//3
+
+//int[] list = { 10, 11, 89, 34, 5, 67, 12, 55, 32, 90 };
+//int[] newlist = new int[10];
+
+//for (int i = 0; i < list.Length; i++)
+//{
+//    newlist[i] = list[i];
+//}
+
+//for (int j = 0; j < list.Length; j++)
+//{
+//    Console.WriteLine(newlist[j]);
+//}
+
+
 
 //4
 
 //int number = 0;
-//int min = 0;
+//int index = 0;
+//int[] list = { 10, 11, 89, 34, 5, 67, 12, 55, 32, 90 };
+
 //Console.Write("Enter your number:");
 //number = int.Parse(Console.ReadLine());
-//min=number;
-//for (int i = 0; i < 19; i++)
+
+//Console.Write("Enter your number index:");
+//index = int.Parse(Console.ReadLine());
+
+//for (int i = 0; i < list.Length; i++)
 //{
-//    Console.Write("Enter your number:");
-//    number=int.Parse(Console.ReadLine());
-//    if (number<min)
+//    if (i == index)
 //    {
-//        min = number;
+//        list[i] = number; 
+//        break;
 //    }
 //}
-//Console.WriteLine("Smallest number:"+min);
+
+//for (int j = 0; j < list.Length; j++)
+//{
+//    Console.WriteLine(list[j]);
+//}
+
+
 
 //5
 
-//int number = 0;
-//int max = 0;
-//Console.Write("Enter your number:");
-//number = int.Parse(Console.ReadLine());
-//max = number;
-//for (int i = 0; i < 19; i++)
+//int check = 0;
+//int count = 0;
+//String repetition="" ;
+//int[] list = { 10, 11, 10, 34, 11 };
+
+//for (int i = 0; i < list.Length; i++)
 //{
-//    Console.Write("Enter your number:");
-//    number = int.Parse(Console.ReadLine());
-//    if (number > max)
+//    check = list[i];
+//    for (int j = 0; j < list.Length; j++)
 //    {
-//        max = number;
+//        if (check == list[j])
+//        {
+//            count++;
+//        }
 //    }
+//    if (count >1)
+//    {
+//        repetition += check + "" + "*";
+//    }
+//    count = 0;
 //}
-//Console.WriteLine("Largest number:" + max);
+
+//Console.WriteLine(repetition);
+
+
 
 //6
 
-//int number = 0;
-//int sum = 0;
-//double avg = 0;
+//int[] list1 = { 67, 33, 10, 34, 11 };
+//int[] list2 = { 22, 90, 10, 5, 11 };
+//int checklist = 0;
+//int count = 0;
+//String repetition = "";
 
-//for (int i = 0; i < 20; i++)
+//for (int i = 0; i < list1.Length; i++)
 //{
-//    Console.Write("Enter your number:");
-//    number = int.Parse(Console.ReadLine());
+//    checklist=list1[i];
+//    for (int j = 0; j < list2.Length; j++)
+//    {
+//        if (checklist == list2[j])
+//        {
+//            count++;
+//        }
+//    }
 
-//    sum += number;
+//    if (count > 0)
+//    {
+//        repetition += checklist + "" + "*";
+//    }
+//    count = 0;
 //}
 
-//avg = sum / 20;
-//Console.WriteLine("Average numbers:"+avg);
+//Console.WriteLine(repetition);
+
+
 
 //7
 
-//int number = 0;
-//int Rightdigit = 0;
-//String Rightdigit5 = "";
-//for (int i = 0; i < 20; i++)
-//{
-//    Console.Write("Enter your number:");
-//    number = int.Parse(Console.ReadLine());
+//int check = 0;
+//int count = 0;
+//String repetition = "";
+//int[] list = { 10, 11, 10, 34, 11 };
 
-//    Rightdigit=number%10;
-//    if (Rightdigit == 5)
+//for (int i = 0; i < list.Length; i++)
+//{
+//    check = list[i];
+//    for (int j = 0; j < list.Length; j++)
 //    {
-//        Rightdigit5+=number.ToString()+"-";
+//        if (check == list[j])
+//        {
+//            count++;
+//        }
 //    }
+//    if (count > 1)
+//    {
+//        list[i] = 0; 
+//    }
+//    count = 0;
 //}
-//Console.WriteLine(Rightdigit5);
+
+//for (int i = 0; i<list.Length; i++)
+//{
+//    Console.WriteLine(list[i]);
+//}
+
+
 
 //8
 
-//int number = 0;
-//int sum = 1;
+//int[] list1 = { 67, 33, 10, 34, 11 };
+//int[] list2 = { 67, 33, 10, 34, 11 };
+//bool checklist ;
+//String Checktotal = "";
 
-//Console.Write("Enter your number:");
-//number=int.Parse(Console.ReadLine());
-
-//for (int i = 1; i <= number; i++)
+//if (list1.Length == list2.Length)
 //{
-//    sum = i * sum;
+//    checklist = true;
 //}
-//Console.WriteLine(sum);
-
-//9
-
-//int number = 0;
-//int count = 0;
-//String str = "";
-
-//Console.Write("Enter your number:");
-//number = int.Parse(Console.ReadLine());
-//str =number.ToString();
-//count = str.Length;
-//Console.WriteLine("Number of digits:" + count);
-
-//10
-
-//int number = 0;
-//int len = 0;
-//int start = 0;
-//String str = "";
-//String turns = "";
-//Console.Write("Enter your number:");
-//number = int.Parse(Console.ReadLine());
-// turns=number.ToString();
-//len = turns.Length;
-//for(int i = len-1; i >=0; i--)
+//else 
 //{
-//    str+=turns.Substring(i,1);
-
-//}
-//Console.WriteLine(str);
-
-//11
-
-//int number = 0;
-
-//Console.Write("Enter your number:");
-//number = int.Parse(Console.ReadLine());
-
-//if (number % 2 != 0&&number%3!=0&& number % 5 != 0 
-//    && number % 7 != 0 && 
-//    number!=1 || number == 2)
-//{
-//    Console.WriteLine("First number:" + number);
-//}
-//else
-//{
-//    Console.WriteLine("This is not a first number!!");
+//    checklist = false; 
 //}
 
-//12
-
-//for (int i = 1; i <= 1000; i++)
+//if (checklist)
 //{
-//    if (i% 2 != 0 && i % 3 != 0 && i % 5 != 0
-//    && i % 7 != 0 && i != 1 || i== 2)
+//    for (int i = 0; i < list1.Length; i++)
 //    {
-//        Console.WriteLine(i);
-//    }
-//}
-
-//13
-
-//int len = 0;
-//String str = "";
-//String turns = "";
-
-//for (int i = 1000; i <= 9999; i++)
-//{
-//    turns = i.ToString();
-//    len = turns.Length;
-//    for (int a = len - 1; a >= 0; a--)
-//    {
-//        str += turns.Substring(a, 1);
-//    }
-//    if (str == turns)
-//    {
-//        Console.WriteLine(str);
-//    }
-//    str = "";
-//}
-
-//14
-
-//int number = 0;
-//int sum = 0;
-
-//Console.Write("Enter your number:");
-//number = int.Parse(Console.ReadLine());
-
-//for (int i = 1; i <= number; i++)
-//{
-//    if (i==1||i%2==0)
-//    {
-//        sum += i;
-//    }
-//    else
-//    {
-//        sum -= i;
-//    }
-//}
-//Console.WriteLine(sum);
-
-//15
-
-//16
-
-//int number = 0;
-//int sum = 0;
-//int count = 0;
-
-//Console.Write("Enter your number:");
-//number = int.Parse(Console.ReadLine());
-
-//for (int i = 1; i <= number; i++)
-//{
-//    if (number%i==0)
-//    {
-//        sum += i;
-//        count++;
-//    }
-//}
-//Console.WriteLine("Sum divided:"+sum+"  "+"divisor number:"+count);
-
-//17
-
-//int month = 0;
-//int day = 0;
-//int yearday = 0;
-
-//Console.Write("Enter your day:");
-//day = int.Parse(Console.ReadLine());
-
-//Console.Write("Enter your month:");
-//month = int.Parse(Console.ReadLine());
-
-//if (month<=6)
-//{
-//    yearday = ((month - 1) * 31)+day;
-//    Console.WriteLine(yearday);
-//}
-//else  if (month > 6&&month<12)
-//{
-//    yearday = (6*31)+((month - 7)*30)+day;
-//    Console.WriteLine(yearday);
-//}
-//else if(month==12)
-//{
-//    yearday=(6*31)+(5*30)+day;
-//    Console.WriteLine(yearday);
-//}
-
-//18
-
-//int numberone = 0;
-//int numbertwo = 0;
-//int bmm = 0;
-//Console.Write("Enter your numberone:");
-//numberone = int.Parse(Console.ReadLine());
-
-//Console.Write("Enter your numbertwo:");
-//numbertwo = int.Parse(Console.ReadLine());
-
-//while (numbertwo!=0)
-//{
-//   bmm = numbertwo;
-//    numbertwo=numberone%numbertwo;
-//    numberone = bmm;
-//}
-//Console.WriteLine(bmm);
-
-//19
-
-//int number = 0;
-//int sum = 0;
-//int remainder = 10;
-//int distribution = 1;
-//int len = 0;
-
-//Console.Write("Enter your number:");
-//number = int.Parse(Console.ReadLine());
-
-//len=number.ToString().Length;
-//for(int i = 1; i <= len; i++)
-//{
-//    sum += (number % remainder) / distribution;
-//    remainder *= 10;
-//    distribution *= 10;
-//}
-//Console.WriteLine(sum);
-
-//20
-
-//String name = "";
-//int time = 0;
-//int rights = 0;
-//int sum = 0;
-
-//Console.Write("Enter your name:");
-//name = Console.ReadLine();
-
-//Console.Write("Enter your time:");
-//time = int.Parse(Console.ReadLine());
-
-//Console.Write("Enter your rights:");
-//rights = int.Parse(Console.ReadLine());
-
-//sum = rights * time;
-
-//Console.WriteLine(name+sum);
-
-//21
-
-//for (int i = 0; i <= 1000; i++)
-//{
-//    if (i % 2 == 0)
-//    {
-//        Console.WriteLine(i);
-//    }
-//}
-
-//22
-
-//for (int i = 0; i <= 1000; i++)
-//{
-//    if (i % 9 == 0)
-//    {
-//        Console.WriteLine(i);
-//    }
-//}
-
-//23
-
-//int number = 0;
-//int sum = 0;
-//int remainder = 10;
-//int distribution = 1;
-//int len = 0;
-
-//Console.Write("Enter your number:");
-//number = int.Parse(Console.ReadLine());
-
-//len = number.ToString().Length;
-//for (int i = 1; i <= len; i++)
-//{
-//    sum += (number % remainder) / distribution;
-//    remainder *= 10;
-//    distribution *= 10;
-//}
-
-
-//if (sum % number==0)
-//{
-//    Console.WriteLine("Ok");
-//}
-//else
-//{
-//    Console.WriteLine("Error");
-//}
-
-//24
-
-//int number1 = 0;
-//int number2 = 0;
-
-//Console.Write("Enter your number1:");
-//number1 = int.Parse(Console.ReadLine());
-
-//Console.Write("Enter your number2:");
-//number2 = int.Parse(Console.ReadLine());
-
-//for (int i = number1; i < number2; i++)
-//{
-//    if (i%3 == 0)
-//    {
-//        Console.WriteLine(i);
-//    }
-//}
-
-//25
-
-//int unit=0;
-//int tens = 0;
-//int hundreds = 0;
-//int thousands = 0;
-//for (int i = 1000; i < 9999; i++)
-//{
-//    unit = i % 10;
-//    tens=(i%100) / 10;
-//    hundreds=(i%1000) / 100;
-//    thousands = (i%10000) / 1000;
-//    if (unit%2==0&&tens%2!=0&&hundreds%2==0&&thousands%2!=0)
-//    {
-//        Console.WriteLine(thousands+" "+hundreds+" "+tens+" "+unit);
-//    }
-//}
-
-//26
-
-//int total = 1000;
-//int count = 0;
-
-//for (int i = 0; i <= total; i += 200)
-//{
-//    for (int j = 0; j <= total-i; j+=100)
-//    {
-//        for (int k = 0; k <= total-i-j; k += 50)
+//        if (list1[i] == list2[i])
 //        {
-//            if (i+j+k == total)
-//            {
-//                count++;
-//            }
+//            Checktotal += "*";
 //        }
 //    }
 //}
-//Console.WriteLine(count);
 
-//27
-
-//int m = 0;
-//int n=0;
-//int sum = 0;
-
-//Console.Write("Enter your number1:");
-//m = int.Parse(Console.ReadLine());
-
-//Console.Write("Enter your number2:");
-//n = int.Parse(Console.ReadLine());
-
-//for(int i = 0; i < n; i++)
+//if (Checktotal.Length == list1.Length)
 //{
-//    sum += m;
+//    Console.WriteLine("YES");
 //}
-//Console.WriteLine(sum);
+//else
+//{
+//    Console.WriteLine("NO");
+//}
 
 
 
+//9
 
+//String[] list = {"1", ".", "1", ".", "1" };
+//String[]newlist=new String[list.Length];
+//String index = "";
+
+//for (int i = 0; i < list.Length; i++)
+//{
+//    if (list[i] == ".")
+//    index +=i.ToString();
+//}
+//Console.WriteLine(index);
+
+
+//int j = 0;
+//for (int i = 0; i < list.Length; i++)
+//{
+//    if (list[i]!=".")
+//    {
+//        newlist[j] = list[i];
+//        j++;
+//    }
+//}
+//Console.WriteLine(newlist.Length);
+//while (j < list.Length)
+//{
+//    newlist[j] = ".";
+//    j++;
+//}
+
+//for (int i = 0;i<newlist.Length; i++)
+//{
+//    Console.WriteLine(newlist[i]);
+//}
+
+
+
+//10
+
+//int[] list = { 67, 33, 10, 34, 11 };
+//int[]Invertlist=new int[list.Length];
+//int index = list.Length-1;
+
+//for (int i = 0; i < list.Length; i++)
+//{
+//    Invertlist[index] = list[i];
+//    index--;
+//}
+
+//for (int i = 0;i < list.Length; i++)
+//{
+//    Console.WriteLine(Invertlist[i]);
+//}
+
+
+
+//11
+
+//int[] list = { 67, 33, -10, 34, -11 };
+//int[] Helptlist = new int[list.Length];
+//int right=0;
+//int left=list.Length-1;
+
+//for(int i=0; i<list.Length; i++)
+//{
+//    if (list[i] > 0)
+//    {
+//        Helptlist[right] = list[i];
+//        right++;
+//    }
+//}
+
+//for(int k=0; k<list.Length; k++)
+//{
+//    if (list[k] < 0)
+//    {
+//        Helptlist[left] = list[k];
+//        left--;
+//    }
+//}
+
+//for(int j=0; j<list.Length; j++)
+//{
+//    Console.WriteLine(Helptlist[j]);
+//}
+
+
+
+//12
+
+//String[] student = new String[3];
+//String[] Numberstudent = new String[3];
+//int[]Avgstudent = new int[3];
+//int sum = 0;
+//int avg = 0;
+//String index = "";
+
+//for (int i = 0; i < student.Length; i++)
+//{
+//    Console.Write("Enter your names student:");
+//    student[i] = Console.ReadLine();
+//}
+
+//for (int i = 0; i < Numberstudent.Length; i++)
+//{
+//    Console.Write("Enter your Numberstudents student:");
+//    Numberstudent[i] = Console.ReadLine();
+//}
+
+//for (int i = 0; i <Avgstudent.Length; i++)
+//{
+//    Console.Write("Enter your Avgstudents student:");
+//    Avgstudent[i] = int.Parse(Console.ReadLine());
+//    sum += Avgstudent[i];
+//    avg = sum / Avgstudent.Length;
+//}
+
+//for (int i = 0;i < Avgstudent.Length; i++)
+//{
+//    if(Avgstudent[i] > avg)
+//    {
+//        index += i;
+//    }
+//}
+//Console.WriteLine(index.Length);
+//for (int i = 0;i<student.Length ; i++)
+//{
+//    if (i == int.Parse(index.Substring(index.Length-1, 1))) 
+//    {
+//        Console.WriteLine("Name:" + student[i]);
+//        Console.WriteLine("Numberstudent:"+ Numberstudent[i]);
+//        Console.WriteLine("Avgstudent:"+ Avgstudent[i]);
+//    }
+//}
+
+
+
+//13
+
+//int[] list1 =new int[5];
+//int[] list2 = new int[5];
+//int checklist = 0;
+//int count = 0;
+//String[] repetition = new string[5];
+
+//for(int i=0; i<list1.Length; i++)
+//{
+//    Console.WriteLine("Enter your numberlist1:");
+//    list1[i]=int.Parse(Console.ReadLine());
+//}
+
+//for (int k = 0; k < list1.Length;k++)
+//{
+//    Console.WriteLine("Enter your numberlist2:");
+//    list2[k] = int.Parse(Console.ReadLine());
+//}
+
+
+//for (int i = 0; i < list1.Length; i++)
+//{
+//    checklist = list1[i];
+//    for (int j = 0; j < list2.Length; j++)
+//    {
+//        if (checklist == list2[j])
+//        {
+//            count++;
+//        }
+//    }
+
+//    if (count > 0)
+//    {
+//        for (int j = 0; j < list1.Length; j++)
+//        {
+//            repetition[j] = checklist.ToString();
+//        }
+//    }
+//    count = 0;
+//}
+
+//for (int i = 0;i < list1.Length; i++)
+//{
+//    Console.WriteLine(repetition[i]);
+//}
 
 
